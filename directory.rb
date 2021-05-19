d7d2c57e74de5762d24243a3b5b71fd104868bea
allison <denisselu19@gmail.com>
2021-05-19 14:30:49 -0600

    Align the output

--- directory.rb
+++ directory.rb
@@ -1,38 +1,35 @@
 def input_students
-  puts "Please, enter the names and hobbies of the students. To finish, just hit return twice."
+  puts "Please, enter the names of the students: "
+  puts "To finish, just hit return twice."
 # Create an empty array:
   students = []
 # Get the first name:
   name = gets.chomp
-# Get to know their hobbies:
-  hobbies = gets.chomp  
 # While the name is not empty, repeat this code:
-  while !name.empty? && !hobbies.empty? do
+  while !name.empty? do
   # Add the student hash to the array:
-    students << {name: name, cohort: :november, hobbies: hobbies}
+    students << {name: name, cohort: :november}
     puts "Now, we have #{students.count} students."
 # Get another name from the user:
     name = gets.chomp
-# Get another hobbie from the user:
-    hobbies = gets.chomp
 end
 #return the array of students:
 students
 end
 
 def print_header
-  puts "The Students of Villain Academy"
-  puts "--------------"
+  puts "The Students of Villain Academy".center(50, '*')
+  puts "----------------------------------------------"
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]}, (#{student[:cohort]} cohort, (#{student[:hobbies]}))"
+    puts "#{student[:name]}, (#{student[:cohort]} cohort)".center(50, '/')
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50, '+')
 end
 
 students = input_students
